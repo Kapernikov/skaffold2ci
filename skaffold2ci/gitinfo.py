8be81507dcdec0669711002c0fcc810cb84fed4c
Frank Dekervel <kervel@gmail.com>
2022-03-02 12:18:28 +0100

    better submodule detection

--- skaffold2ci/gitinfo.py
+++ skaffold2ci/gitinfo.py
@@ -12,8 +12,8 @@ def is_submodule(main_project_path: PosixPath, submodule_path: PosixPath) -> boo
 def contains_git_folder(path: PosixPath) -> bool:
     """Check if a path contains a .git folder"""
     for p in path.rglob(".git"):
-        if p.is_dir():
-            return True
+        return True
+    return False
 
 def should_clone_recursive(main_project_path: PosixPath, context: PosixPath) -> bool:
     """Check if a submodule should be cloned recursively"""
