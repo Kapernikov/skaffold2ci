7ecedba65fc6b47e342f618bf7a554803495201d
Frank Dekervel <kervel@gmail.com>
2022-03-02 15:02:51 +0100

    basic auth works!

--- skaffold2ci/skaffold2ci.py
+++ skaffold2ci/skaffold2ci.py
@@ -56,8 +56,9 @@ def freezecharts(ctx, input: str, outputdirectory: str, prefix: str, suffix: str
 @click.option("--outputdirectory", required=True, type=click.Path(dir_okay=True, file_okay=False), help="The output directory that contains the frozen charts")
 @click.option("--gitlab-token", default=os.getenv("CI_JOB_TOKEN",""), type=str, help="gitlab token")
 @click.option("--gitlab-url", default=os.getenv("CI_API_V4_URL",""), type=str, help="gitlab V4 api url")
+@click.option("--gitlab-username", default="gitlab-ci-token", type=str, help="username of deploy token")
 @click.option("--gitlab-project-id", default=os.getenv("CI_PROJECT_ID",""), type=str, help="gitlab project ID")
-def gitlab_upload_frozen_charts(ctx, input: str, outputdirectory: str, gitlab_token: str, gitlab_url: str, gitlab_project_id: str):
+def gitlab_upload_frozen_charts(ctx, input: str, outputdirectory: str, gitlab_token: str, gitlab_url: str, gitlab_project_id: str, gitlab_username: str):
     skaffold = parse_skaffold_yaml(input)
     semver = gitinfo.getSemver(Path(input))
     for release in skaffold.helmReleases():
@@ -67,12 +68,18 @@ def gitlab_upload_frozen_charts(ctx, input: str, outputdirectory: str, gitlab_to
                 files = {"chart": f}
                 data = {"token": gitlab_token}
                 channel = "stable"
-                auth = HTTPDigestAuth("gitlab-ci-token", gitlab_token)
+                auth = HTTPDigestAuth(gitlab_username, gitlab_token)
                 r = requests.post(f"{gitlab_url}/projects/{gitlab_project_id}/packages/helm/api/{channel}/charts", files=files, data=data, auth=auth)
                 if (r.status_code < 200 or r.status_code >= 300):
                     print(r.text)
                     print(r.status_code)
-                    raise Exception("Failed to upload chart")
+                    print("Failed to upload chart, retry with basic auth")
+                auth = HTTPBasicAuth(gitlab_username, gitlab_token)
+                r = requests.post(f"{gitlab_url}/projects/{gitlab_project_id}/packages/helm/api/{channel}/charts", files=files, data=data, auth=auth)
+                if (r.status_code < 200 or r.status_code >= 300):
+                    print(r.text)
+                    print(r.status_code)
+                    raise Exception("Failed to upload chart, both digest and basic auth failed")
 
 
 
